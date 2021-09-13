5742f3e14d7abbe83e37c569e998d7c0bac5d46f
Michael Cavanaugh <funkel1989@gmail.com>
2021-09-12 22:40:45 -0400

    email extensions and typo work

--- EmailValidator/Extensions/EmailExtensions.cs
+++ EmailValidator/Extensions/EmailExtensions.cs
@@ -1,10 +1,71 @@
 namespace EmailValidator.Extensions
 {
+    using System;
+    using System.Linq;
+
     public static class EmailExtensions
     {
         public static string GetEmailDomain(this string email)
         {
-            return email.Split('@')[1];
+            return email.Split("@")[1];
+        }
+
+        public static (string, string, string, string) SplitEmail(this string email)
+        {
+            if (string.IsNullOrWhiteSpace(email))
+            {
+                throw new ArgumentNullException(nameof(email));
+            }
+            email = email.Trim();
+
+            var emailParts = email.Split("@");
+
+            if (emailParts.Length < 2 || email.Any(x => string.IsNullOrWhiteSpace(x.ToString())))
+            {
+                throw new ArgumentException($"{email} is not a valid email");
+            }
+
+            var domain = emailParts.Last();
+            var domainParts = domain.Split(".");
+            var secondLevelDomain = string.Empty;
+            var topLevelDomain = string.Empty;
+
+            switch (domainParts.Length)
+            {
+                case 0:
+                    throw new ArgumentException("Email is missing a top level domain");
+                case 1:
+                    topLevelDomain = domainParts[0];
+                    break;
+                default:
+                {
+                    secondLevelDomain = domainParts[0];
+                    for (var i = 1; i < domainParts.Length; i++)
+                    {
+                        topLevelDomain += domainParts[i] + ".";
+                    }
+
+                    topLevelDomain = topLevelDomain.Substring(0, topLevelDomain.Length - 1);
+                    break;
+                }
+            }
+
+            var address = string.Join("@", emailParts);
+
+            return (topLevelDomain, secondLevelDomain, domain, address);
+        }
+
+        public static string EncodeEmail(this string email)
+        {
+            // http://en.wikipedia.org/wiki/Email_address#Syntax
+            var result = Uri.EscapeUriString(email);
+            return result.Replace("%20", "")
+                .Replace("%25", "%")
+                .Replace("%5E", "^")
+                .Replace("%60", "`")
+                .Replace("%7B", "{")
+                .Replace("%7C", "|")
+                .Replace("%7D", "}");
         }
     }
 }
--- EmailValidator/Validators/TypoCheck.cs
+++ EmailValidator/Validators/TypoCheck.cs
@@ -1,6 +1,7 @@
 namespace EmailValidator.Validators
 {
     using System.Collections.Generic;
+    using Models;
 
     public class TypoCheck
     {
@@ -8,22 +9,29 @@ namespace EmailValidator.Validators
         private readonly int _secondLevelThreshold = 2;
         private readonly int _topLevelThreshold = 2;
 
-        private List<string> _defaultDomains = new()
+        private readonly List<string> _defaultDomains = new()
         {
             "msn.com", "bellsouth.net", "telus.net", "comcast.net", "optusnet.com.au", "earthlink.net", "qq.com",
             "sky.com", "icloud.com", "mac.com", "sympatico.ca", "googlemail.com", "att.net", "xtra.co.nz", "web.de",
             "cox.net", "gmail.com", "ymail.com", "aim.com", "rogers.com", "verizon.net", "rocketmail.com", "google.com",
             "optonline.net", "sbcglobal.net", "aol.com", "me.com", "btinternet.com", "charter.net", "shaw.ca"
         };
-        private List<string> _defaultSecondLevelDomains =
+        private readonly List<string> _defaultSecondLevelDomains =
             new() { "yahoo", "hotmail", "mail", "live", "outlook", "gmx" };
 
-        private List<string> _defaultTopLevelDomains = new()
+        private readonly List<string> _defaultTopLevelDomains = new()
         {
             "com", "com.au", "com.tw", "ca", "co.nz", "co.uk", "de", "fr", "it", "ru", "net", "org", "edu", "gov", "jp",
             "nl", "kr", "se", "eu", "ie", "co.il", "us", "at", "be", "dk", "hk", "es", "gr", "ch", "no", "cz", "in",
             "net", "net.au", "info", "biz", "mil", "co.jp", "sg", "hu", "uk"
         };
+
+        public TypoCheck(TypoOptions options)
+        {
+            _defaultDomains = options.Domains ?? _defaultDomains;
+            _defaultSecondLevelDomains = options.SecondLevelDomains ?? _defaultSecondLevelDomains;
+            _defaultTopLevelDomains = options.TopLevelDomains ?? _defaultTopLevelDomains;
+        }
 
     }
 }
